88bd5f4389edd44c991fc44f0643c21d43cfbe0e
Muhammad Rafikov <49823186+TheOneWhoWill@users.noreply.github.com>
2022-06-21 11:44:35 -0400

    Removed Header and setup init.js

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ function App() {
 	return (
 			<NavigationContainer>
 				<Stack.Navigator
-					screenOptions={{headerTitleAlign: 'center'}}
+					screenOptions={{headerShown: false}}
 				>
 					{currentUser ? 
 						<Stack.Screen
--- firebase/init.js
+++ firebase/init.js
@@ -1,12 +1,18 @@
-import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+import { initializeApp, getApp, getApps } from "firebase/app";
 
-const app = initializeApp({
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-})
+const firebaseConfig = {
+  // Paste In your own, I'm not stupid
+}
 
-export default app
+let app;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp()
+}
+
+const auth = getAuth();
+
+export { auth };
